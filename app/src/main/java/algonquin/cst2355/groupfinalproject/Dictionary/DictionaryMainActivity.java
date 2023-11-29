fbb9ddb6f34b2dd373600b286b9ee703c87de206
ziyanghuang <huan0269@algonquinlive.com>
2023-11-29 03:41:35 -0500

    add help button

--- app/src/main/java/algonquin/cst2355/groupfinalproject/Dictionary/DictionaryMainActivity.java
+++ app/src/main/java/algonquin/cst2355/groupfinalproject/Dictionary/DictionaryMainActivity.java
@@ -2,18 +2,22 @@ package algonquin.cst2355.groupfinalproject.Dictionary;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import algonquin.cst2355.groupfinalproject.MainActivity;
 import algonquin.cst2355.groupfinalproject.R;
 import algonquin.cst2355.groupfinalproject.databinding.ActivityMainBinding;
 import algonquin.cst2355.groupfinalproject.databinding.DictionaryActivityMainBinding;
@@ -25,7 +29,7 @@ public class DictionaryMainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_dictionary, menu);
         return true;
     }
 
@@ -70,6 +74,29 @@ public class DictionaryMainActivity extends AppCompatActivity {
                     .setNegativeButton("No", (dialog, cl)->{} )
                     .create().show();
         });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch( item.getItemId() )
+        {
+            // this case allows going to dictionary by clicking the icon
+            case R.id.dictionary_icon:
+
+            case R.id.help:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Help")
+                        .setMessage("Enter a word then click the Search button. The app will help you find" +
+                                "the dictionary definition for the word.")
+                        .setPositiveButton("Thanks",(dialog,cl)->{
+                        })
+                        .create().show();
+                //put other go-to icons here
+                break;
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
